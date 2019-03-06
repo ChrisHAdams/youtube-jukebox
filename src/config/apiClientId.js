e78ec8c04ce4dbfe392c7a56e05a8135d28e6c69
ChrisHAdams <chris.adams@lucentitservices.co.uk>
2019-03-06 23:43:01 +0000

    ignore files

--- src/config/apiClientId.js
+++
@@ -1,3 +0,0 @@
-const ApiClientId = '';  // Add your Google Client Id here.
-
-export default ApiClientId;
--- src/config/apiKey.js
+++
@@ -1,3 +0,0 @@
-const ApiKey = ''; // Add your Google API Key here.
-
-export default ApiKey;
